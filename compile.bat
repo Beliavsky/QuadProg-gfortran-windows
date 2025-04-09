61dffb24e0646439b6c05f563810d78b2df67a58
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2025-04-09 19:48:03 -0400

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,4 @@
+:: gfortran constants.f90 quadprog\solvers.f90 QuadProg.f90 x.f90 librefblas.a
+:: gfortran constants.f90  ..\build\dependencies\test-drive\src\testdrive.F90 quadprog\solvers.f90 QuadProg.f90 ..\test\problems.f90 x.f90 librefblas.a
+:: gfortran constants.f90  ..\build\dependencies\test-drive\src\testdrive.F90 quadprog\solvers.f90 QuadProg.f90 ..\test\problems.f90 ..\test\check.f90 librefblas.a
+gfortran constants.f90 testdrive.F90 solvers.f90 QuadProg.f90 problems.f90 check.f90 librefblas.a
