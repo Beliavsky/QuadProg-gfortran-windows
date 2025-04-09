496fcb2ad88573c937ff638cdd19ca7ae6e5d471
Beliavsky <beliavsky@aol.com>
2025-04-09 19:55:46 -0400

    revised compilation commands

--- compile.bat
+++ compile.bat
@@ -1,4 +1,2 @@
-:: gfortran constants.f90 quadprog\solvers.f90 QuadProg.f90 x.f90 librefblas.a
-:: gfortran constants.f90  ..\build\dependencies\test-drive\src\testdrive.F90 quadprog\solvers.f90 QuadProg.f90 ..\test\problems.f90 x.f90 librefblas.a
-:: gfortran constants.f90  ..\build\dependencies\test-drive\src\testdrive.F90 quadprog\solvers.f90 QuadProg.f90 ..\test\problems.f90 ..\test\check.f90 librefblas.a
-gfortran constants.f90 testdrive.F90 solvers.f90 QuadProg.f90 problems.f90 check.f90 librefblas.a
+gfortran -c constants.f90 Quadprog.f90
+gfortran constants.o testdrive.F90 solvers.f90 QuadProg.o problems.f90 check.f90 librefblas.a
