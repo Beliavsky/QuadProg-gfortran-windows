9d307281f81cbbdbc8fe0d832bcf67392d77593a
Beliavsky <beliavsky@aol.com>
2025-04-09 19:56:55 -0400

    named executable

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 gfortran -c constants.f90 Quadprog.f90
-gfortran constants.o testdrive.F90 solvers.f90 QuadProg.o problems.f90 check.f90 librefblas.a
+gfortran -o a.exe constants.o testdrive.F90 solvers.f90 QuadProg.o problems.f90 check.f90 librefblas.a
